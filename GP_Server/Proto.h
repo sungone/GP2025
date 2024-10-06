2da9fda71e219886647bde38eead950e1dd4f161
sujissi <hsjgame@tukorea.ac.kr>
2024-10-06 22:24:24 +0900

    Update Proto.h

--- GP_Server/Proto.h
+++ GP_Server/Proto.h
@@ -1,16 +1,29 @@
-#pragma once
 constexpr short SERVER_PORT = 4000;
 constexpr int BUFSIZE = 1024;
 constexpr int MAX_CLIENT = 1000;
 
-using int32 = int;   // int32¸? int·? ?¤??
+using int32 = int;   // int32를 int로 정의
 
-enum class EPacketType : uint16_t
+enum class EPacketType : uint8_t
 {
 	C_LOGIN,
 	C_LOGINOUT,
 	C_MOVE,
-	S_LOGININFO
+
+	S_LOGININFO,
+	S_ADD_PLAYER,
+	S_REMOVE_PLAYER,
+	S_MOVE_PLAYER,
+};
+
+struct FVectorInfo
+{
+	float X;
+	float Y;
+	float Z;
+	float Yaw;
+	float Pitch;
+	float Roll;
 };
 
 #pragma pack(push, 1)
@@ -18,7 +31,7 @@ enum class EPacketType : uint16_t
 struct FPacketHeader
 {
 	EPacketType PacketType;
-	uint16_t PacketSize;
+	uint8_t PacketSize;
 };
 
 struct FLoginPacket
@@ -31,24 +44,21 @@ struct FLoginInfoPacket
 {
 	FPacketHeader Header;
 	int32 PlayerID;
-	float X;
-	float Y;
-	float Z;
-	float Yaw;
-	float Pitch;
-	float Roll;
+	FVectorInfo VecInfo;
 };
 
 struct FMovePacket
 {
 	FPacketHeader Header;
 	int32 PlayerID;
-	float X;
-	float Y;
-	float Z;
-	float Yaw;
-	float Pitch;
-	float Roll;
+	FVectorInfo VecInfo;
+};
+
+struct FAddPlayerPacket
+{
+	FPacketHeader Header;
+	int32 PlayerID;
+	FVectorInfo VecInfo;
 };
 
 #pragma pack(pop)
