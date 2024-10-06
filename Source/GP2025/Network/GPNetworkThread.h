30f0d345f474a60cd6736683437c01dc3ff78eb0
sujissi <hsjgame@tukorea.ac.kr>
2024-10-06 13:55:32 +0900

    클라 recv 비동기로

---
+++ Source/GP2025/Network/GPNetworkThread.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FSocket;
+
+class GP2025_API GPNetworkThread :public FRunnable
+{
+public:
+	GPNetworkThread(FSocket* Socket);
+	~GPNetworkThread() {};
+	
+	virtual uint32 Run() override;
+	void Destroy() { isThreadRuning = false; };
+private:
+	FRunnableThread* Thread = nullptr;
+	FSocket* Socket;
+	bool isThreadRuning = true;
+};
