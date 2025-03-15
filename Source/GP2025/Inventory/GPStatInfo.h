0f196bcbc353aa56a00672357d049530e8aad631
sungone <sungwon000604@gmail.com>
2025-03-16 03:46:11 +0900

    Stat Info

---
+++ Source/GP2025/Inventory/GPStatInfo.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GPStatInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP2025_API UGPStatInfo : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
