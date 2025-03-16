04f491eae607db6d70c0cc0fffbf323d42c6355b
sungone <sungwon000604@gmail.com>
2025-03-16 21:04:20 +0900

    로그인 창 로비 창

---
+++ Source/GP2025/UI/GPButton.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "GPButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP2025_API UGPButton : public UButton
+{
+	GENERATED_BODY()
+	
+};
