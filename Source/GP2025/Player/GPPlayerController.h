06284db85692de90de1000322ad9df078a9d1480
sungone <sungwon000604@gmail.com>
2025-03-20 01:38:51 +0900

    일단 인게임 UI 올림

--- Source/GP2025/Player/GPPlayerController.h
+++ Source/GP2025/Player/GPPlayerController.h
@@ -17,4 +17,36 @@ class GP2025_API AGPPlayerController : public APlayerController
 	
 protected :
 	virtual void BeginPlay() override;
+
+public:
+	AGPPlayerController();
+
+public:
+	UPROPERTY()
+	class UGPInGameWidget* InGameWidget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UGPInGameWidget> InGameWidgetClass;
+
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void ShowInGameUI();
+public:
+	UPROPERTY()
+	class UGPLoginWidget* LoginWidget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UGPLoginWidget> LoginWidgetClass;
+
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void ShowLoginUI();
+
+public :
+	UPROPERTY()
+	class UGPLobbyWidget* LobbyWidget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UGPLobbyWidget> LobbyWidgetClass;
+
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void ShowLobbyUI();
 };
