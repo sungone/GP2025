a66e6f342a374600ec535d88d1fb2834b077300c
sungone <sungwon000604@gmail.com>
2025-03-20 15:41:42 +0900

    수정

--- Source/GP2025/UI/GPInGameWidget.h
+++ Source/GP2025/UI/GPInGameWidget.h
@@ -14,38 +14,38 @@ class GP2025_API UGPInGameWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
-public :
-public:
-    virtual void NativeConstruct() override;
-
-    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
-    class UProgressBar* HealthBar;
-
-    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
-    class UProgressBar* ExpBar;
-
-    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
-    class UTextBlock* PlayerLevelText;  
-
-    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
-    class UProgressBar* QSkillBar;
-
-    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
-    class UProgressBar* ESkillBar;
-
-    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
-    class UProgressBar* RSkillBar;
-
-    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
-    class UTextBlock* CurrentLocation;
-	
-
-public :
-    void UpdateHealthBar(float Ratio);
-    void UpdateExpBar(float Ratio);
-    void UpdatePlayerLevel(int32 NewLevel);
-
-
-public :
-    class AGPCharacterMyplayer* MyPlayer;
+//public :
+//public:
+//    virtual void NativeConstruct() override;
+//
+//    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
+//    class UProgressBar* HealthBar;
+//
+//    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
+//    class UProgressBar* ExpBar;
+//
+//    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
+//    class UTextBlock* PlayerLevelText;  
+//
+//    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
+//    class UProgressBar* QSkillBar;
+//
+//    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
+//    class UProgressBar* ESkillBar;
+//
+//    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
+//    class UProgressBar* RSkillBar;
+//
+//    UPROPERTY(meta = (BindWidget), BlueprintReadWrite, Category = "UI")
+//    class UTextBlock* CurrentLocation;
+//	
+//
+//public :
+//    void UpdateHealthBar(float Ratio);
+//    void UpdateExpBar(float Ratio);
+//    void UpdatePlayerLevel(int32 NewLevel);
+//
+//
+//public :
+//    class AGPCharacterMyplayer* MyPlayer;
 };
