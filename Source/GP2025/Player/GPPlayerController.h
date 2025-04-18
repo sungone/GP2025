0d60f2d0cca042270082633360faa7a078789264
sungone <sungwon000604@gmail.com>
2025-04-19 04:26:46 +0900

    삭제

--- Source/GP2025/Player/GPPlayerController.h
+++ Source/GP2025/Player/GPPlayerController.h
@@ -21,32 +21,4 @@ protected :
 public:
 	AGPPlayerController();
 
-public:
-	UPROPERTY()
-	class UGPInGameWidget* InGameWidget;
-
-	UPROPERTY(EditDefaultsOnly, Category = "UI")
-	TSubclassOf<UGPInGameWidget> InGameWidgetClass;
-
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	void ShowInGameUI();
-public:
-	UPROPERTY()
-	class UGPLoginWidget* LoginWidget;
-
-	UPROPERTY(EditDefaultsOnly, Category = "UI")
-	TSubclassOf<UGPLoginWidget> LoginWidgetClass;
-
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	void ShowLoginUI();
-
-public :
-	UPROPERTY()
-	class UGPLobbyWidget* LobbyWidget;
-
-	UPROPERTY(EditDefaultsOnly, Category = "UI")
-	TSubclassOf<UGPLobbyWidget> LobbyWidgetClass;
-
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	void ShowLobbyUI();
 };
