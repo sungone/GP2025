36874860c91a4bc9ea1b65496da0a5f587db618e
sungone <sungwon000604@gmail.com>
2025-05-02 21:09:05 +0900

    ufunction

--- Source/GP2025/Character/GPCharacterNPC.h
+++ Source/GP2025/Character/GPCharacterNPC.h
@@ -35,7 +35,8 @@ public :
 	UFUNCTION()
 	void OnPlayerEnter(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
+	
+	UFUNCTION()
 	void OnPlayerExit(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
