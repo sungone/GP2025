593205369d719a16f062e88d62416e242ee262d6
sungone <sungwon000604@gmail.com>
2025-06-12 22:26:19 +0900

    CH1_GO_TO_BUNKER

---
+++ Source/GP2025/Quest/GPQuestTriggerBox.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GPQuestTriggerBox.h"
+#include "Character/GPCharacterMyplayer.h"
+#include "Network/GPNetworkManager.h"
+
+AGPQuestTriggerBox::AGPQuestTriggerBox()
+{
+	OnActorBeginOverlap.AddDynamic(this, &AGPQuestTriggerBox::OnOverlapBegin);
+}
+
+void AGPQuestTriggerBox::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGPQuestTriggerBox::OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (AGPCharacterMyplayer* MyPlayer = Cast<AGPCharacterMyplayer>(OtherActor))
+	{
+		if (!MyPlayer->NetMgr)
+			return;
+
+		// Quest 분기 처리
+		switch (TriggerQuestType)
+		{
+		case 4:
+			MyPlayer->NetMgr->SendMyCompleteQuest(QuestType::CH1_GO_TO_BUNKER);
+			break;
+
+			// 추후 확장 가능
+		default:
+			UE_LOG(LogTemp, Warning, TEXT("QuestTriggerBox: AssignedQuest not handled."));
+			break;
+		}
+	}
+}
---
+++ Source/GP2025/Quest/GPQuestTriggerBox.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "GPQuestTriggerBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP2025_API AGPQuestTriggerBox : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	AGPQuestTriggerBox();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION()
+	void OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+	uint8 TriggerQuestType;
+	
+};
