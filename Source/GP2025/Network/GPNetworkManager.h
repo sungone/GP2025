cf49f226ce1453bf9c58393bba8809ba0500ffb0
sujissi <120546504+sujissi@users.noreply.github.com>
2025-07-21 16:28:55 +0900

    Update GPNetworkManager.h

--- Source/GP2025/Network/GPNetworkManager.h
+++ Source/GP2025/Network/GPNetworkManager.h
@@ -104,8 +104,8 @@ private:
 	void ReceiveData();
 private:
 	class FSocket* Socket;
-	FString IpAddress = SERVER_IP;
-	//FString IpAddress = "211.188.61.31";
+	//FString IpAddress = SERVER_IP;
+	FString IpAddress = "211.188.61.31";
 	int16 Port = SERVER_PORT;
 
 	UPROPERTY()
